357805619b5639b29e68528ec20fb9fd6f0bb6eb
William Quan <William@Williams-MacBook-Pro.local>
2017-05-26 00:35:11 -0700

    adding all files

---
+++ test/math-test.py
@@ -0,0 +1,7 @@
+import unittest
+from src.math import Math
+
+class MathTest(unittest.TestCase):
+    def test_addition(self):
+        # Make Test fail
+        self.assertEqual(Math.addition(3,4), 8)
